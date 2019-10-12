e1a6d7e90c8ccec0119c137057000ff78a1910c1
Felipe Palazzo <palazzofelipe@gmail.com>
2019-10-12 14:00:02 +0100

    Add fonts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './App.css'
+import './styles/main.scss'
 import Navigation from './components/Navigation'
 
 function App() {
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,12 +6,14 @@ import arrow from '../../assets/img/arrow-down-dark.svg'
 export default function Navigation() {
 	return (
 		<header className="header">
-			<nav className="navbar">
-				<a href="https://monese.com/">Monese</a>
-				<div type="navbar__option">
-					<img src={flag} alt="" />
-					<span>English</span>
-					<img src={arrow} alt="" />
+			<nav className="navbar container">
+				<div className="navbar__content col-xs-12">
+					<a href="https://monese.com/">Monese</a>
+					<div type="navbar__option">
+						<img src={flag} alt="" />
+						<span className="navbar__option__text">English</span>
+						<img src={arrow} alt="" />
+					</div>
 				</div>
 			</nav>
 		</header>
