f4c9a3dd0d7e21bc498c3f9ab6685816a2f7509c
Felipe Palazzo <palazzofelipe@gmail.com>
2019-10-13 12:25:39 +0100

    More styling

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './Button.scss'
+import arrowRight from '../../assets/img/arrow-right-white.svg'
 
 export default function Button(props) {
-	return <button onClick={props.onClick}>{props.children}</button>
+	return (
+		<button
+			className="button"
+			onClick={props.onClick}
+			type="button"
+			disabled={props.disabled}
+		>
+			<span>{props.children}</span>
+			<img src={arrowRight} alt="" className="button__icon" />
+		</button>
+	)
 }
 
 Button.propTypes = {
 	children: PropTypes.string,
-	onClick: PropTypes.func.isRequired
+	onClick: PropTypes.func,
+	disabled: PropTypes.bool
 }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import countries from '../../data/countries.json'
 import List from '../List'
+import Button from '../Button'
 import { stringToCssClass } from '../../helpers'
 import arrow from '../../assets/img/arrow-down-dark-17.svg'
 import './Form.scss'
@@ -18,38 +19,46 @@ export default function Form() {
 	return (
 		<>
 			<section className="form-container">
-				<div>
-					<form className="form">
-						<div
-							className="form__code-container"
-							onClick={toggleList}
-						>
-							<span
-								className={`flag flag--md flag--${stringToCssClass(
-									item.country
-								)}`}
-							></span>
-							<span className="form__code">{`+${item.code}`}</span>
-							<img
-								src={arrow}
-								className={`form__arrow form__arrow--${
-									visible ? 'up' : 'down'
-								}`}
-								alt=""
+				<section className="form-content">
+					<div>
+						<form className="form">
+							<div
+								className="form__code-container"
+								onClick={toggleList}
+							>
+								<span
+									className={`flag flag--md flag--${stringToCssClass(
+										item.country
+									)}`}
+								></span>
+								<span className="form__code">{`+${item.code}`}</span>
+								<img
+									src={arrow}
+									className={`form__arrow form__arrow--${
+										visible ? 'up' : 'down'
+									}`}
+									alt=""
+								/>
+							</div>
+							<input
+								type="text"
+								className="form__input"
+								placeholder="Your phone number"
 							/>
-						</div>
-						<input
-							type="text"
-							className="form__input"
-							placeholder="Your phone number"
-						/>
-					</form>
-					<span className="form-container__text">
-						Use the one linked to your Monese account
-					</span>
-				</div>
+						</form>
+						<span className="form-content__text">
+							Use the one linked to your Monese account
+						</span>
+					</div>
+				</section>
+				<List data={countries} onClick={onClick} isVisible={visible} />
 			</section>
-			<List data={countries} onClick={onClick} isVisible={visible} />
+			<section className="button-container">
+				<Button>Next</Button>
+				<span className="button-container__text">
+					Don’t have an account?
+				</span>
+			</section>
 		</>
 	)
 }
