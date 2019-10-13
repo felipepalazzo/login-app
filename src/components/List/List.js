313e5a3e81cf4955dc9713eafcc72beb1545f69e
Felipe Palazzo <palazzofelipe@gmail.com>
2019-10-13 16:58:21 +0100

    Add filter

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { stringToCssClass } from '../../utils/helpers'
 import './List.scss'
 
 export default function List({ data, onClick, isVisible }) {
+	const [items, setItems] = useState(data)
+	const onValueChange = ({ target }) => {
+		const value = target.value.toLowerCase()
+		const filteredItems = data.filter(
+			item => item.country.substr(0, value.length).toLowerCase() === value
+		)
+		setItems(filteredItems)
+	}
 	return (
 		<div className="list-container">
 			<section className={`list-box ${isVisible ? 'active' : 'hidden'}`}>
+				<input
+					type="text"
+					name="country"
+					className="list__input"
+					autoComplete="false"
+					onChange={onValueChange}
+				/>
 				<ul className="list">
-					{data.map(item => (
+					{items.map(item => (
 						<li
 							className="list__item"
 							key={item.code}
